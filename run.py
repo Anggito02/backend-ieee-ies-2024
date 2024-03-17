29a162e736d6a80d7165b268451244d15656233d
Benedictus Wicaksono <benewicaksono@gmail.com>
2024-03-17 22:07:28 +0700

    feat: allow CORS

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from flaskr import create_app
+from flask_cors import CORS
 
 app = create_app()
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 if __name__ == '__main__':
     app.run(debug=True)
