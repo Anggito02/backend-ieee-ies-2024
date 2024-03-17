2406d84c740a11940abdbb407313e0cd2ea63215
Anggito02 <anggito_anju@yahoo.com>
2024-03-17 22:48:21 +0700

    activate ngrok

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
+import ngrok
+
 from flaskr import create_app
 from flask_cors import CORS
 
 app = create_app()
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+
+listener = ngrok.forward(5000, authtoken_from_env=True)
+
+print(f"Ingress established at {listener.url()}")
 
 if __name__ == '__main__':
     app.run(debug=True)
